f736178968b4da31a82dbf45789069470657445a
Parzival-IOI <119472721+Parzival-IOI@users.noreply.github.com>
2023-03-13 01:11:12 +0700

    test2

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export default createClient (
-    process.env.DB_URL!,
-    process.env.DB_PASS!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
 )
