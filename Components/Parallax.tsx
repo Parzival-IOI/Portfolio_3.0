1887431fa50aeb8d360afecffd4197525c18ebdf
Parzival-IOI <119472721+Parzival-IOI@users.noreply.github.com>
2023-03-31 22:27:43 +0700

    parallax_fixed

--- Components/Parallax.tsx
+++ Components/Parallax.tsx
@@ -5,9 +5,9 @@ import seaF from '../app/SeaF.png'
 
 const Parallax = () => {
   return (
-    <section className='relative w-full h-screen overflow-hidden'>
-        <Image src={sea} alt="" width={1000} height={1000} className="w-full h-full fixed -z-10 bg-no-repeat" ></Image>
-        <Image src={seaF} alt="" width={1000} height={1000} className="w-full h-full " ></Image>
+    <section className='relative h-[120vh] '>
+        <Image src={sea} alt="" width={1000} height={1000} className="md:w-full md:h-auto h-screen w-auto fixed -z-10 bg-no-repeat object-cover bg-center top-0 " ></Image>
+        <Image src={seaF} alt="" width={1000} height={1000} className="md:w-full md:h-auto h-screen w-auto object-cover bg-center" ></Image>
     </section>
   )
 }
