e94592bb4d2049412f38d1b7357a5639c5174963
Parzival-IOI <119472721+Parzival-IOI@users.noreply.github.com>
2023-04-01 15:14:01 +0700

    intro

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Parallax from '@/components/Parallax'
-
+import Intro from '@/components/Intro'
 
 const page = () => {
   return (
     <div className='min-h-[120vh] '>
       <Parallax></Parallax>
+      <Intro></Intro>
     </div>
   )
 }
