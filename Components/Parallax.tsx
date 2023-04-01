14d94eee709f0d71c26b74e74e7747e5b5ba4204
Parzival-IOI <119472721+Parzival-IOI@users.noreply.github.com>
2023-04-01 15:02:58 +0700

    fixed ?

--- Components/Parallax.tsx
+++ Components/Parallax.tsx
@@ -7,7 +7,7 @@ const Parallax = () => {
   return (
     <section className='h-[120vh] '>
         <Image src={sea} alt="" width={1000} height={1000} className="md:w-full md:h-auto h-screen w-auto fixed -z-10 bg-no-repeat object-cover bg-center top-0 " ></Image>
-        <Image src={seaF} alt="" width={1000} height={1000} className="md:w-full md:h-auto h-screen w-auto object-cover bg-center" ></Image>
+        <Image src={seaF} alt="" width={1000} height={1000} className="md:w-full md:h-auto h-screen w-auto object-cover bg-center top-0" ></Image>
     </section>
   )
 }
