c75b5ee3ea4c0e719a1eee56bfc072a56ee649ef
Parzival-IOI <119472721+Parzival-IOI@users.noreply.github.com>
2023-04-09 23:36:39 +0700

    test project page

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import supabase from '@/utils/supabase'
 
-export const revalidate = 0;
+export const revalidate = 3600;
+
+type DataInfo = {
+  id: number,
+  created_at: Date,
+  name: string,
+  language: string,
+  picture: string,
+  date: Date,
+  link: string,
+  description: string,
+}
 
 const page = async () => {
-    const {data} = await supabase.from("CardProject").select("*");
+    const data = await supabase.from("CardProject").select("*");
   return (
     <pre>
         {JSON.stringify(data, null, 2)}
