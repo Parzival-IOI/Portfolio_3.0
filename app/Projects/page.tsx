b278fd375f1b504820159e0a37d08c8be1b1e03e
Parzival-IOI <119472721+Parzival-IOI@users.noreply.github.com>
2023-06-09 18:43:26 +0700

    Change animation on the home page

--- app/Projects/page.tsx
+++ app/Projects/page.tsx
@@ -4,20 +4,20 @@ import { forEachChild } from 'typescript';
 
 export const revalidate = 3600;
 
-interface DataInfo {
-  id: number,
-  created_at: Date,
-  name: string,
-  language: string,
-  picture: string,
-  date: Date,
-  link: string,
-  description: string,
+type DataInfo = {
+  id?: number,
+  created_at?: Date,
+  name?: string,
+  language?: string,
+  picture?: string,
+  date?: Date,
+  link?: string,
+  description?: string,
 }
 
 const page = async () => {
-    const { data} = await supabase.from("CardProject").select("*");
-
+    const { data, error }:{ data:DataInfo[] | undefined | null, error:any} = await supabase.from("CardProject").select("*");
+    console.log(data);
     return (
     <div>
       <pre>
