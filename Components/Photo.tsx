85e61af73db0c6bc1b05e6b9cd56e362c2d4315d
Parzival-IOI <119472721+Parzival-IOI@users.noreply.github.com>
2023-07-01 17:12:09 +0700

    changing project/card radient

--- Components/Photo.tsx
+++ Components/Photo.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image, { StaticImageData } from 'next/image'
 import React from 'react'
 import php from '@/public/php.png'
 import python from '@/public/python.png'
@@ -18,29 +18,90 @@ interface PicName {
   name? : string
 }
 
+interface varibleConst {
+  image : StaticImageData;
+  text : string,
+  background : string,
+}
+
+
 const Photo: React.FC<Props>  = (props) => {
-
-  function getPic(name?:string | undefined) {
-    let alpha;
-    if(name == "c_cpp") alpha = c_cpp;
-    else if (name == "react") alpha = react;  
-    else if (name == "php") alpha = php;
-    else if (name == "python") alpha = python;
-    else if (name == "micropython") alpha = micropython;
-    else if (name == "cs") alpha = cs;
-    else if (name == "ino") alpha = ino;
-    else if (name == "java") alpha = java;
-    else if (name == "hcj") alpha = hcj;
-    else alpha = NoImage;
-
-    return alpha;
-  }
-
   return (
     <div className='w-full h-[45%] overflow-hidden'>
-      <Image src={getPic(props.pic)} alt="" width={1000} height={1000} className="w-full h-full object-cover bg-gradient-to-br from-green-500 to-cyan-600 group-hover:scale-110 transition-translate duration-500"></Image>
+      <Image src={variable[getPic(props.pic)].image} alt="" width={1000} height={1000} className={`${"w-full h-full object-cover group-hover:scale-110 transition-translate duration-500"} ${variable[getPic(props.pic)].background}`}></Image>
     </div>
   )
 }
 
 export default Photo
+
+function getPic(name?:string | undefined) {
+  let alpha:number;
+
+  if(name == "c_cpp") alpha = 0;
+  else if (name == "react") alpha = 1;
+  else if (name == "php") alpha = 2;
+  else if (name == "python") alpha = 3;
+  else if (name == "micropython") alpha = 4;
+  else if (name == "cs") alpha = 5;
+  else if (name == "ino") alpha = 6;
+  else if (name == "java") alpha = 7;
+  else if (name == "hcj") alpha = 8;
+  else alpha = 9;
+
+  return alpha;
+}
+
+
+const variable:varibleConst[] = [
+  {
+    image : c_cpp,
+    text : "",
+    background: "",
+  },
+  {
+    image : react,
+    text : "",
+    background: " bg-gradient-to-br from-green-500 to-cyan-600 ",
+  },
+  {
+    image : php,
+    text : "",
+    background: "",
+  },
+  {
+    image : python,
+    text : "",
+    background: "",
+  },
+  {
+    image : micropython,
+    text : "",
+    background: "",
+  },
+  {
+    image : cs,
+    text : "",
+    background: "",
+  },
+  {
+    image : ino,
+    text : "",
+    background: "",
+  },
+  {
+    image : java,
+    text : "",
+    background: "",
+  },
+  {
+    image : hcj,
+    text : "",
+    background: "",
+  },
+  {
+    image : NoImage,
+    text : "",
+    background: "",
+  },
+]
