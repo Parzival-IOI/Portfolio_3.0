3c9d74e4b872f2c19a6c2312c30d0c90bf2e46d3
Parzival-IOI <119472721+Parzival-IOI@users.noreply.github.com>
2023-08-01 15:56:49 +0700

    Responsive_Menu

--- Components/Icon.tsx
+++ Components/Icon.tsx
@@ -98,6 +98,26 @@ export function Arrow() {
     )
 }
 
+export function Menu() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className="bi bi-card-list" viewBox="0 0 16 16">
+            <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
+            <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"/>
+        </svg>
+    )
+}
+
+export function Close() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
+            <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+        </svg>
+    )
+}
+
+
+
+
 
 
 
--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -1,9 +1,10 @@
 "use client"
 
 import Link from 'next/link'
-import { HomeIcon } from './Icon'
+import { Close, HomeIcon, Menu } from './Icon'
 import { usePathname } from 'next/navigation'
 import { Inter } from 'next/font/google'
+import { useState } from 'react'
 
 const inter = Inter({
   subsets : ['latin'],
@@ -37,9 +38,17 @@ const name: NavName[] = [
 const Navbar = () => {
   let pathname = usePathname() || '/';
   let isActive : boolean;
+  const [sideBar, setSideBar] = useState(false)
+
+  const openSideBar = ()=> {
+    setSideBar((e)=>!e)
+    if(sideBar) {
+
+    }
+  }
 
   return (
-    <div className={`${inter.className} w-full h-16 md:h-20 dark:bg-slate-700/50 bg-slate-400/60 flex items-center justify-between z-50 absolute`}>
+    <nav className={`${inter.className} w-full h-16 md:h-20 dark:bg-slate-700/50 bg-slate-400/60 flex items-center justify-between z-50 absolute`}>
       <div className=' h-full aspect-square rounded-full grid place-items-center '>
         <div className='p-2 md:p-3 aspect-square rounded-full grid place-items-center dark:hover:text-yellow-400 hover:border-2 dark:hover:border-yellow-300 hover:text-rose-600 hover:border-rose-500 transition-all duration-200'>
           {isActive = ("/" === pathname) ? true : false}
@@ -48,14 +57,20 @@ const Navbar = () => {
           </Link>
         </div>
       </div>
-      <div className='flex gap-2 md:gap-4 lg:gap-6 mr-6 font-bold'>
+      <button type='button' onClick={()=>{openSideBar()}} className={`block sm:hidden mr-6 transition-all duration-500 ${sideBar ? `fixed right-0 z-50` : `` }`}>
+        {!sideBar && <Menu/>}
+        {sideBar && <Close/>}
+      </button>
+      {sideBar && <div className='fixed left-0 top-0 w-full h-full bg-slate-800/50 delay-200 transition-opacity duration-500' onClick={()=>{openSideBar()}}></div>}
+      
+      <div className={`fixed w-1/2 h-full top-0 bg-slate-800/95 transition-all duration-500  flex justify-center items-center flex-col gap-y-16 text-xl mr-6 lg:gap-6 font-bold sm:bg-transparent sm:flex-row sm:w-auto sm:relative sm:left-0 sm:text-base sm:gap-4 ${sideBar ? `left-1/2` : `left-full` }`}>
         {name.map((items)=>{
           isActive = (items.path === pathname) ? true : false;
           return (
-            <Link href={{pathname: items.path}} key={items.key} className={`px-1 py-1 hover:dark:text-slate-300 hover:text-slate-800 relative transition-all duration-300 before:content-[''] before:transition-all before:absolute before:bottom-0 before:left-0 before:rounded-md before:h-[8%] before:dark:bg-white before:bg-blue-900  ${isActive ? `before:w-full` : `before:w-0 hover:before:w-full`}`}>{items.name}</Link>
+            <Link onClick={()=>{openSideBar()}} href={{pathname: items.path}} key={items.key} className={`px-1 py-1 hover:dark:text-slate-300 hover:text-slate-800 relative transition-all duration-300 before:content-[''] before:transition-all before:absolute before:bottom-0 before:left-0 before:rounded-md before:h-[8%] before:dark:bg-white before:bg-blue-900  ${isActive ? `before:w-full` : `before:w-0 hover:before:w-full`}`}>{items.name}</Link>
         )})}
       </div>
-    </div>
+    </nav>
   )
 }
 
